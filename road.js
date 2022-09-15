2c26ceab0fd5000cd6a76a12d613fc6ccf26c9cb
dilan-js <dilannana.js@gmail.com>
2022-09-15 06:12:46 -0500

    changed borders for scaleability incase I want to curve roads etc.

--- road.js
+++ road.js
@@ -10,11 +10,21 @@ class Road {
     const infinity = 1000000; //innate JS infinity causes some issues so using explicit value
     this.top = -infinity;
     this.bottom = infinity;
+
+    const topLeft = { x: this.left, y: this.top };
+    const topRight = { x: this.right, y: this.top };
+    const bottomLeft = { x: this.left, y: this.bottom };
+    const bottomRight = { x: this.right, y: this.bottom };
+    //we only have 2 borders, but could have multiple i.e. highways
+    this.borders = [
+      [topLeft, bottomLeft],
+      [topRight, bottomRight],
+    ];
   }
 
-  //returns for different line indices, an offset of laneWidth away from
+  // returns for different line indices, an offset of laneWidth away from
   // middle of first lane
-  // we do Math.min to place car in right-most lane even if I specify I want it to be placed
+  // Error Prevention: we do Math.min to place car in right-most lane even if I specify I want it to be placed
   // further to the right (prevents car placed outside road)
   getLaneCenter(laneIndex) {
     const laneWidth = this.width / this.laneCount;
@@ -30,17 +40,23 @@ class Road {
     ctx.strokeStyle = "white";
 
     //drawing lines for lanes using linear interpolation
-    for (let i = 0; i <= this.laneCount; i++) {
+    for (let i = 1; i <= this.laneCount - 1; i++) {
       const x = lerp(this.left, this.right, i / this.laneCount);
-      if (i > 0 && i < this.laneCount) {
-        ctx.setLineDash([20, 20]); //the dash will be 20px, then break for 20px etc.
-      } else {
-        ctx.setLineDash([]); //this is for borders
-      }
+
+      ctx.setLineDash([20, 20]); //the dash will be 20px, then break for 20px etc.
+
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+
+    ctx.setLineDash([]);
+    this.borders.forEach((border) => {
+      ctx.beginPath();
+      ctx.moveTo(border[0].x, border[0].y);
+      ctx.lineTo(border[1].x, border[1].y);
+      ctx.stroke();
+    });
   }
 }
