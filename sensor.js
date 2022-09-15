63948c1864a0c9bbd76fc0728bc2a6f45eba2946
dilan-js <dilannana.js@gmail.com>
2022-09-15 06:40:36 -0500

    functionality for acknlowedging potential collisions

--- sensor.js
+++ sensor.js
@@ -2,17 +2,59 @@
 class Sensor {
   constructor(car) {
     this.car = car;
-    this.rayCount = 1;
+    this.rayCount = 10;
     this.rayLength = 150; //sensors have a range, after which they don't work
     this.raySpread = Math.PI / 4; //45 degrees span for rays
     this.rays = [];
+
+    this.sensorReadings = []; //telling if there is a border or not, how far is it
   }
 
-  update() {
+  update(roadBorders) {
+    this.#castRays();
+    this.sensorReadings = [];
+
+    for (let i = 0; i < this.rayCount; i++) {
+      this.sensorReadings.push(this.#getReadings(this.rays[i], roadBorders));
+    }
+  }
+
+  //We only have 2 borders, so 1 ray can only touch both borders if car goes off screen
+  //However, with traffic, could cause issues
+  //Find closest border and use that as reading
+  #getReadings(ray, roadBorders) {
+    let touches = [];
+
+    for (let i = 0; i < roadBorders.length; i++) {
+      const touch = getIntersection(
+        ray[0],
+        ray[1],
+        roadBorders[0],
+        roadBorders[1]
+      );
+      //if null i.e. segments don't intersect, no reading
+      if (touch) {
+        touches.push(touch);
+      }
+
+      if (touches.length == 0) {
+        return null;
+      } else {
+        //return all the offsets of all the touches
+        const offsets = touches.map((touch) => touch.offset);
+        //find minimum offset b/c that's closest collision
+        const minOffset = Math.min(...offsets);
+        return touches.find((touch) => touch.offset == minOffset); //returns the reading with min we found
+      }
+    }
+  }
+
+  #castRays() {
     this.rays = [];
     for (let i = 0; i < this.rayCount; i++) {
       //lerp gives value between A & B depending on t
       // max value for i = raycount -1 in this case
+      // if rayCount = 1, then would throw error, division by 0, so set default
       const rayAngle =
         lerp(
           this.raySpread / 2,
