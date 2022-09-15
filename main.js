7590336dea5ce8138bf5eb1a188620b11651cd18
dilan-js <dilannana.js@gmail.com>
2022-09-15 05:50:57 -0500

    added lanes

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ canvas.width = 200;
 //To draw on the canvas, we need drawing 2D drawing context
 //It will contain all the methods we need to draw what we want
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(100, 100, 30, 50);
 
 animate();
@@ -12,6 +13,7 @@ animate();
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
