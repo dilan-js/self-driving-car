b4130a519fbb0dd39e31e2d38a69cc553a4dfab4
dilan-js <dilannana.js@gmail.com>
2022-09-15 10:08:28 -0500

    added parallelization to show the best case scenarios for a self driving instance

--- main.js
+++ main.js
@@ -7,31 +7,54 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "MAIN_CHARACTER_AI");
+const N = 100;
+const cars = generateCars(N);
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "NPC", 2)];
 
 animate();
+
+function generateCars(N) {
+  const cars = [];
+  for (let i = 1; i < N; i++) {
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "MAIN_CHARACTER_AI"));
+  }
+  return cars;
+}
 
 function animate(time) {
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, []);
   }
-  car.update(road.borders, traffic);
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].update(road.borders, traffic);
+  }
+
+  //creating new array with only y-values
+  //finding most promising car moving forward
+  //centers canvas on car that is moving forward
+  const bestCar = cars.find(
+    (car) => car.y == Math.min(...cars.map((c) => c.y))
+  );
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
 
   carCtx.save();
-  carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
 
   road.draw(carCtx);
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].draw(carCtx, "purple");
   }
-  car.draw(carCtx, "blue");
+  carCtx.globalAlpha = 0.14;
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].draw(carCtx, "blue");
+  }
+  carCtx.globalAlpha = 1;
+  bestCar.draw(carCtx, "blue", true);
 
   carCtx.restore();
   networkCtx.lineDashOffset = -time / 50;
-  Visualizer.drawNetwork(networkCtx, car.brain);
+  Visualizer.drawNetwork(networkCtx, bestCar.brain);
   requestAnimationFrame(animate);
 }
